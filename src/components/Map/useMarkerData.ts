5b426daaf24407c335f9eeaf116236cad47ef7fc
Tomas <tomas@ramanauskas.co.uk>
2024-04-20 21:16:08 +0100

    Setting map center to user location

--- src/components/Map/useMarkerData.ts
+++ src/components/Map/useMarkerData.ts
@@ -1,4 +1,5 @@
 import { LatLngExpression, Map } from 'leaflet'
+import IPinfoWrapper from 'node-ipinfo'
 import { useEffect, useMemo, useState } from 'react'
 
 import useMapContext from '#components/Map/useMapContext'
@@ -27,7 +28,6 @@ const useMarkerData = ({ locations, map, viewportWidth, viewportHeight }: useMap
   // get bounds of all markers
   const allMarkerBounds = useMemo(() => {
     if (!locations || !leafletLib) return undefined
-
     const coordsSum: LatLngExpression[] = []
     locations.forEach(item => {
       coordsSum.push(item.position)
@@ -45,21 +45,44 @@ const useMarkerData = ({ locations, map, viewportWidth, viewportHeight }: useMap
       acc[category].push(location)
       return acc
     }, {})
-
     const mappedClusters = Object.keys(groupedLocations).map(key => ({
       category: Number(key),
       markers: groupedLocations[key],
     }))
-
     return mappedClusters
   }, [locations])
 
+  // Fetch user's location based on IP address
+  useEffect(() => {
+    const fetchUserLocation = async () => {
+      const token = process.env.NEXT_PUBLIC_IPINFO_TOKEN
+      if (!token) {
+        // Handle the error or log it using an appropriate logging mechanism
+        return
+      }
+      const ipinfoWrapper = new IPinfoWrapper(token)
+      try {
+        const response = await ipinfoWrapper.lookupIp('')
+        const { loc } = response
+        if (loc) {
+          const [latitude, longitude] = loc.split(',').map(parseFloat)
+          setAllMarkersBoundCenter(prevState => ({
+            ...prevState,
+            centerPos: [latitude, longitude] as LatLngExpression,
+          }))
+        }
+      } catch (error) {
+        // Handle the error or log it using an appropriate logging mechanism
+      }
+    }
+    fetchUserLocation()
+  }, [])
+
   // auto resize map to fit all markers on viewport change
-  // it's crucial to set viewport size as dependecy to trigger the map resize
+  // it's crucial to set viewport size as dependency to trigger the map resize
   useEffect(() => {
     if (!allMarkerBounds || !map) return
     if (!viewportWidth || !viewportHeight) return
-
     map.invalidateSize()
     setAllMarkersBoundCenter({
       minZoom: map.getBoundsZoom(allMarkerBounds),
